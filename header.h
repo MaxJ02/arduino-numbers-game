45899462a70c83018003723d8eb67e51284965e8
Max <114068285+MaxJ02@users.noreply.github.com>
2023-03-15 13:39:24 +0100

    Update header.h

--- header.h
+++ header.h
@@ -3,15 +3,35 @@
 
 #include "plusheader.hpp"
 
-#define counter1
-#define timer1
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdio.h>
+
+#define F_CPU 16000000UL
 
 /********************************************************************************
-* setup: Initiates the game startup screen.
+* setup: Initiates the game and displays the startup screen.
 ********************************************************************************/
 void setup(void);
 
 
+/********************************************************************************
+* setup: Initiates all ports.
+********************************************************************************/
+void init_ports(void);
+
+
+/********************************************************************************
+* setup: Initiates timer1 and the necessary code.
+********************************************************************************/
+void init_timer(void);
+  
+
+/********************************************************************************
+* setup: Initiates the game startup screen.
+********************************************************************************/
+void start_countdown(void);  
 
 
 #endif /* HEADER_H_ */
